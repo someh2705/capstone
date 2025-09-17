6d726c1c94b5b581b14175ba542f01f74ea2df44
someh2705 <someh2705@gmail.com>
2025-09-17 19:06:07 +0000

    initialize commit

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "capstone project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgsFor = system: import nixpkgs {
+        inherit system;
+      };
+
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = pkgsFor system;
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              gcc
+              clang-tools
+
+              ninja
+              cmake
+              pkg-config
+              neocmakelsp
+
+              ns-3
+              python3
+            ];
+
+            shellHook =''
+              echo "ns-3 development environment activated!"  
+            '';
+          };
+        });
+    };
+}
